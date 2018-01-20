9fe1d091f969fc68b9ab6289f97091960ea787fa
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-01-20 15:12:44 +0700

    Add endpoint create location with route parameter

--- src/main/java/com/ysn/springmvc/controller/location/LocationController.java
+++ src/main/java/com/ysn/springmvc/controller/location/LocationController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -71,7 +68,44 @@ public class LocationController {
         boolean isFieldValid = true;
 
         if (location.getDatetime() == null) {
-            diagnostic.setMessage("Field name is required");
+            diagnostic.setMessage("Field datetime is required");
+            isFieldValid = false;
+        } else if (location.getLatitude() == 0.0) {
+            diagnostic.setMessage("Field latitude is required");
+            isFieldValid = false;
+        } else if (location.getLongitude() == 0.0) {
+            diagnostic.setMessage("Field longitude is required");
+            isFieldValid = false;
+        } else if (location.getSpeed() < 0) {
+            diagnostic.setMessage("Field speed is required");
+            isFieldValid = false;
+        }
+
+        if (!isFieldValid) {
+            diagnostic.setStatus(HttpStatus.BAD_REQUEST.value());
+            mapDataReturn.put("diagnostic", diagnostic);
+            return new ResponseEntity<>(mapDataReturn, HttpStatus.BAD_REQUEST);
+        } else {
+            diagnostic.setStatus(HttpStatus.CREATED.value());
+            diagnostic.setMessage(HttpStatus.CREATED.name());
+            mapDataReturn.put("diagnostic", diagnostic);
+        }
+        mapDataReturn.put("data", location);
+        locationRepository.save(location);
+        return new ResponseEntity<>(mapDataReturn, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Map<String, Object>> createLocation(@RequestParam String datetime, @RequestParam  double latitude, @RequestParam  double longitude, @RequestParam  int speed) {
+        Location location = new Location(0, datetime, latitude, longitude, speed);
+        log("createLocation by route parameter: " + location);
+        Map<String, Object> mapDataReturn = new HashMap<>();
+        Diagnostic diagnostic = new Diagnostic();
+        diagnostic.setUnix_timestamp(new Date().getTime());
+        boolean isFieldValid = true;
+
+        if (location.getDatetime() == null) {
+            diagnostic.setMessage("Field datetime is required");
             isFieldValid = false;
         } else if (location.getLatitude() == 0.0) {
             diagnostic.setMessage("Field latitude is required");
